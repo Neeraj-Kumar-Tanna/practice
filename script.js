9a5ffd8c2c67d55fd444d4cc449f7fa3379056e8
Neeraj kumar <neeraj772006@gmal.com>
2026-05-21 10:37:47 +0530

    changes in js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let ul = document.querySelector("ul");
 ul.addEventListener("click",(x) =>{
     x.target.classList.toggle("line");
 });
+
+let c = document.querySelector("#circle");
+c.addEventListener("mouseover" , function(){
+    c.style.backgroundColor = "black";
+});
+c.addEventListener("mouseout" , function(){
+    c.style.backgroundColor = "red";
+});
