2ccf2f8343fb869ec1d810002b32361192ec53b3
Neeraj kumar <neeraj772006@gmal.com>
2026-05-26 15:31:12 +0530

    added loading animation using js time intervals

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-let form = document.querySelector("form");
-let inp = document.querySelectorAll("input");
+let fill = document.querySelector("#fill");
+let perc = document.querySelector("#percentage");
+let count = 0;
 
-form.addEventListener("submit" , function(x){
-    x.preventDefault();
+let x = setInterval(function(){
+    if(count <= 99){
+        count++;
+        fill.style.width = `${count}%`;
+        perc.innerText = `${count}%`
+    }
+    else{
+        document.querySelector("#downloading").innerText = "Downloaded";
+        clearInterval(x);
+    }
+} , 10000 / 100);
 
-    inp.forEach(val => {
-        if(val.type == "email"){
-            let regex = "^[a-zA-Z0-9._%+-]+@gmail\.com$";
-            let ans = val.value.match(regex);
-            if(!ans){
-                document.querySelector("#email_alert").style.display="initial";
-            }
-            else{
-                document.querySelector("#email_alert").style.display="none";
-                console.log(val.value);
-            }
-        }
-        else if(val.type == "number"){
-            if(val.value < 0 || val.value >100){
-                console.log("invalid age");
-            }
-            else{
-                console.log("Age : " + val.value);
-            }
-        }
-        else if(val.type != "submit"){
-            console.log(val.value);
-        }
-    });
 
-    console.log(document.querySelector("select").value);
-})
+
+let reset = document.querySelector("#reset");
+
+reset.addEventListener("click" , function(){
+    document.querySelector("#downloading").innerText = "Downloading........";
+    count = 0 ;
+    let x = setInterval(function(){
+    if(count <= 99){
+        count++;
+        fill.style.width = `${count}%`;
+        perc.innerText = `${count}%`
+    }
+    else{
+        document.querySelector("#downloading").innerText = "Downloaded";
+        clearInterval(x);
+    }
+    } , 10000 / 100);
+});
