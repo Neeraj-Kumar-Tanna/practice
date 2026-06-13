061ff2c3b95b8657eb9940fdf4be3138b8060c94
Neeraj kumar <neeraj772006@gmal.com>
2026-06-13 22:28:44 +0530

    adding items option created to the search bar

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ inp.addEventListener("input" , ()=>{
     }
 });
 
+let btn = document.querySelector("button");
+btn.addEventListener("click" , function(){
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.setAttribute("data-name" , document.querySelector("#name").value);
+    document.querySelector(".cards-container").appendChild(div);
+    
+    let image = document.createElement("img");
+    image.classList.add("card-image");
+    image.setAttribute("src" , document.querySelector("#image_url").value);
+    div.appendChild(image);
+    
+    let name = document.createElement("h3");
+    name.classList.add("card-title");
+    name.innerText = document.querySelector("#name").value;
+    div.appendChild(name);
+    
+    let disc = document.createElement("p");
+    disc.classList.add("card-description");
+    disc.innerText = document.querySelector("#description").value;
+    div.appendChild(disc);
+    
+    console.log("clicked...");
+
+
+});
