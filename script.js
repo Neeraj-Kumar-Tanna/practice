43c2e71c147d376dac5aab2c249154ab2757e1a5
Neeraj kumar <neeraj772006@gmail.com>
2026-06-16 20:38:14 +0530

    feat: profile adding done

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
+let userName = document.querySelector("#name");
+let role = document.querySelector("#role");
+let discription = document.querySelector("#discription");
 
+let form = document.querySelector("form"); // Select the form, not the button
 
-function login(){
-    let password;
-    return {
-        create(att){
-            password = att;
-            console.log("passwoed set");
-        },
-        check(att){
-            if(password === att){
-                console.log("correct password");
-            }
-            else{
-                console.log("wrong password");
-            }
-        }
-    }
+let obj ={
+    users : [],
+    createUser(){
+        form.addEventListener("submit" , (e) => this.addData(e)); // Pass function reference
+    },
+    addData(e){
+        e.preventDefault(); // Use the event parameter
+        this.users.push({
+            userName : userName.value,
+            role : role.value,
+            discription : discription.value
+
+        });
+
+        this.renderUi();
+
+        form.reset();
+    },
+    renderUi(){
+        
+        let card = document.createElement("div");
+        card.classList.add("card");
+        document.querySelector("#profiles").appendChild(card);
+
+        let h2 = document.createElement("h2");
+        h2.innerText = userName.value;
+        card.appendChild(h2);
+
+        let h3 = document.createElement("h3");
+        h3.innerText = role.value;
+        card.appendChild(h3); 
+        
+        let p = document.createElement("p");
+        p.innerText = discription.value;
+        card.appendChild(p); 
+
+        let btn = document.createElement("button");
+        btn.classList.add("delete");
+        card.appendChild(btn);
+        
+
+    },
+    removeUser(){}
 }
 
-let person1 = login();
-person1.create("neeraj123");
-person1.check("neeraj1234");
+obj.createUser(); // Initialize the event listener
