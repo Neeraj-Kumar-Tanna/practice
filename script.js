1897066188287e88cf1aaccd6787cfc0aca8ac53
Neeraj kumar <neeraj772006@gmail.com>
2026-06-26 23:15:32 +0530

    Module pattern (iife)

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-fetch("https://randomuser.me/api/?results=100")
-.then(function(raw){
-    return raw.json();
-})
-.then(function(data){
-    data.results.forEach((ele) => {
+let bank = (function(){
+    let balance = 2000;
 
-        console.log(ele)
-        
-        
-        const card = document.createElement("div");
-        card.className = "card";
+    function checkBalance(){
+        console.log(balance);
+    }
 
-        // Create image
-        const img = document.createElement("img");
-        img.src = ele.picture.large;
-         img.alt = "Profile Picture";
-         img.className = "profile-pic";
+    function setBalance(val){
+        balance = val;
+        console.log(balance + "setted");
+    }
 
-        const name = document.createElement("h3");
-        name.innerText = `${ele.name.first} ${ele.name.last}`;
-        
+    return {
+        checkBalance,
+        setBalance
+    };
+})();
 
-// Create info div
-const info = document.createElement("div");
-info.className = "info";
-
-// Create email
-const email = document.createElement("p");
-email.innerHTML = `<strong>Email:</strong> ${ele.email}`;
-
-// Create gender
-const gender = document.createElement("p");
-gender.innerHTML = `<strong>Gender:</strong> ${ele.gender}`;
-
-// Append elements
-info.append(email, gender);
-card.append(img,name ,info);
-
-// Add card to page
-document.body.appendChild(card);
-    });
-})
-.catch(function(error){
-    console.log(error);
-});
-
+bank.checkBalance();
+bank.setBalance(30000);
+bank.checkBalance();
