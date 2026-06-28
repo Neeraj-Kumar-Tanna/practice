32cea4fc11d83ed4295678d4ae1932f3e8cb6eab
Neeraj kumar <neeraj772006@gmail.com>
2026-06-28 22:31:49 +0530

    feat: debounce for search bar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let inp = document.querySelector("#searchInput");
+let names = document.querySelectorAll(".card");
 
 let search = () =>{
     let value = inp.value;
@@ -29,3 +31,30 @@ let db_search = db(search , 2000);
 
 // inp.addEventListener("input" , db(search , 3000));
 inp.addEventListener("input" , db_search);
+
+let btn = document.querySelector("button");
+btn.addEventListener("click" , function(){
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.setAttribute("data-name" , document.querySelector("#name").value);
+    document.querySelector(".cards-container").appendChild(div);
+    
+    let image = document.createElement("img");
+    image.classList.add("card-image");
+    image.setAttribute("src" , document.querySelector("#image_url").value);
+    div.appendChild(image);
+    
+    let name = document.createElement("h3");
+    name.classList.add("card-title");
+    name.innerText = document.querySelector("#name").value;
+    div.appendChild(name);
+    
+    let disc = document.createElement("p");
+    disc.classList.add("card-description");
+    disc.innerText = document.querySelector("#description").value;
+    div.appendChild(disc);
+    
+    console.log("clicked...");
+
+
+});
