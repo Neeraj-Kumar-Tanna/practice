f4ce185f48cb30b3fa0b1e69d0df0b42fc82d313
Neeraj kumar <neeraj772006@gmail.com>
2026-07-01 17:54:39 +0530

    noraml import method

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
+import { dis } from "./heavy.js";
 
 let btn = document.querySelector("#heavyLoading");
-
-btn.addEventListener("click", async function () {
-    let x = await import("./heavy.js");
-    // let h = document.querySelector("#heavy");
-    // h.src = h.dataset.src;
-    x.dis();
+btn.addEventListener("click" , ()=>{
+    dis();
 });
