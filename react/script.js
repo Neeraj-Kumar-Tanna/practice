25eba6b074c07ed3aad596832b0ab3ad1de2a40d
Neeraj kumar <neeraj772006@gmail.com>
2026-08-18 13:37:32 +0530

    prac: React.createElement and createRoot

---
+++ react/script.js
@@ -0,0 +1,5 @@
+import {h1 , div} from "./shortcuts.js";
+
+var main =  ReactDOM.createRoot(document.querySelector("#main"));
+
+main.render(div(h1("neeraj") , h1("manasa") , h1("diyansha") , h1("dathrisri")));
---
+++ react/shortcuts.js
@@ -0,0 +1,3 @@
+export const h1 = (name)=> React.createElement("h1" , null , `hi ${name}!!`);
+
+export const div = (...args) => React.createElement("div" , null , [...args]);
